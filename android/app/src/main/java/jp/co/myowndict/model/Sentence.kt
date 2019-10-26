b7ecf48ce7e1ff76ec8a28bc715b81a32aea5e5d
shiita0903 <shiita0903@gmail.com>
2019-10-27 00:18:53 +0900

    add Sentence list.

--- android/app/src/main/java/jp/co/myowndict/model/Sentence.kt
+++ android/app/src/main/java/jp/co/myowndict/model/Sentence.kt
@@ -2,9 +2,11 @@ package jp.co.myowndict.model
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import kotlin.random.Random
 
 @JsonClass(generateAdapter = true)
 data class Sentence(
     @Json(name = "content_jp") val contentJp: String,
-    @Json(name = "content_en") val contentEn: String
+    @Json(name = "content_en") val contentEn: String,
+    val count: Int = Random.nextInt() % 20   // 用途不明。とりあえずランダム値を設定
 )
