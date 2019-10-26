daaeaf2e522b5b22e52327ce2c7988f0ebdbe07a
clockvoid <hidenari.t1997nov@gmail.com>
2019-10-26 16:09:28 +0900

    add: permission check when access to microphone

--- android/app/src/main/java/jp/co/myowndict/view/main/MainFragment.kt
+++ android/app/src/main/java/jp/co/myowndict/view/main/MainFragment.kt
@@ -1,28 +1,29 @@
 package jp.co.myowndict.view.main
 
+import android.Manifest
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat.startForegroundService
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.lifecycle.lifecycleOwner
 import com.wada811.databinding.dataBinding
 import dagger.android.support.DaggerFragment
 import jp.co.myowndict.R
 import jp.co.myowndict.databinding.FragmentMainBinding
 import jp.co.myowndict.speechrecognize.SpeechRecognizeService
-import jp.co.myowndict.view.splash.SplashViewModel
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import jp.co.myowndict.view.startAppSettingActivity
+import permissions.dispatcher.*
 import timber.log.Timber
 import javax.inject.Inject
 
+@RuntimePermissions
 class MainFragment : DaggerFragment() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -42,10 +43,11 @@ class MainFragment : DaggerFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.viewPager.adapter = MainFragmentPagerAdapter(this)
 
-        startSpeechRecording()
+        startSpeechRecordingWithPermissionCheck()
     }
 
-    private fun startSpeechRecording() {
+    @NeedsPermission(Manifest.permission.RECORD_AUDIO)
+    fun startSpeechRecording() {
         speechService = Intent(requireContext(), SpeechRecognizeService::class.java)
         if (SpeechRecognizeService.isRunning.not()) {
             startForegroundService(requireContext(), speechService)
@@ -58,6 +60,49 @@ class MainFragment : DaggerFragment() {
         } catch (e: Exception) {
             Timber.d(e)
         }
+    }
+
+    @OnShowRationale(Manifest.permission.RECORD_AUDIO)
+    fun showRationaleForContacts(request: PermissionRequest) {
+        MaterialDialog(requireContext()).show {
+            title(text = "マイクへのアクセスを許可してください")
+            message(text = "録音を開始するには，マイクへのアクセスを許可する必要があります")
+            positiveButton(text = "OK") { request.proceed() }
+            lifecycleOwner(viewLifecycleOwner)
+            cancelable(false)
+        }
+    }
+
+    @OnPermissionDenied(Manifest.permission.RECORD_AUDIO)
+    fun onContactsDenied() {
+        MaterialDialog(requireContext()).show {
+            message(text = "録音を開始するには，マイクへのアクセスを許可する必要があります")
+            positiveButton(text = "OK")
+            lifecycleOwner(viewLifecycleOwner)
+            cancelable(false)
+        }
+    }
+
+    @OnNeverAskAgain(Manifest.permission.RECORD_AUDIO)
+    fun onContactsNeverAskAgain() {
+        MaterialDialog(requireContext()).show {
+            message(text = "録音を開始するには，マイクへのアクセスを許可する必要があります。設定画面を開きますか？")
+            positiveButton(text = "OK") {
+                startAppSettingActivity()
+            }
+            negativeButton(text = "戻る")
+            lifecycleOwner(viewLifecycleOwner)
+            cancelable(false)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        onRequestPermissionsResult(requestCode, grantResults)
     }
 
     class MainFragmentPagerAdapter(
