516d2c3bb194a8c237d7c016b7baaae825a475ca
yusk <tdfagamdb@yahoo.co.jp>
2019-10-26 18:15:27 +0900

    feat: add sentence delete API

--- server/main/views/api/user_sentence.py
+++ server/main/views/api/user_sentence.py
@@ -1,14 +1,22 @@
-from rest_framework.mixins import (RetrieveModelMixin, ListModelMixin)
+from django.db.models import Count
+
+from rest_framework.mixins import (RetrieveModelMixin, ListModelMixin,
+                                   DestroyModelMixin)
 from rest_framework.viewsets import GenericViewSet
 
 from main.models import Sentence
 from main.serializers import SentenceSerializer
 
 
-class UserSentenceViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
+class UserSentenceViewSet(ListModelMixin, RetrieveModelMixin,
+                          DestroyModelMixin, GenericViewSet):
     serializer_class = SentenceSerializer
+
+    def perform_destroy(self, instance):
+        instance.usersentence_set.filter(text__user=self.request.user).delete()
 
     def get_queryset(self):
         res = Sentence.objects.filter(
-            usersentence__text__user=self.request.user).distinct()
+            usersentence__text__user=self.request.user).distinct().annotate(
+                Count('usersentence')).order_by("-usersentence__count")
         return res
