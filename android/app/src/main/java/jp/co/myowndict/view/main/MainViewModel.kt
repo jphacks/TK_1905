1e8fa176a1596c1fe4226ff476009f7bfd1fa242
tks10 <tktktks10@gmail.com>
2019-10-26 17:15:36 +0900

    feat: viewmodel method

--- android/app/src/main/java/jp/co/myowndict/view/main/MainViewModel.kt
+++ android/app/src/main/java/jp/co/myowndict/view/main/MainViewModel.kt
@@ -1,9 +1,12 @@
 package jp.co.myowndict.view.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import jp.co.myowndict.data.Repository
 import jp.co.myowndict.extensions.launchWithProgress
 import jp.co.myowndict.model.Result
+import jp.co.myowndict.model.Sentence
 import jp.co.myowndict.view.ApiFragmentViewModel
 import timber.log.Timber
 import javax.inject.Inject
@@ -11,11 +14,24 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val repository: Repository
 ) : ApiFragmentViewModel() {
+    private val sentencesLiveData: MutableLiveData<List<Sentence>> = MutableLiveData()
+    val sentences: LiveData<List<Sentence>>
+        get() = sentencesLiveData
+
     fun sendSpeechText(text: String) {
         viewModelScope.launchWithProgress(inProgressLiveData) {
             when (val result = repository.sendText(text)) {
                 is Result.Success -> Timber.d("Sent text -> $text")
                 is Result.Error -> Timber.e("Failed to send text -> $text")
+            }
+        }
+    }
+
+    fun getSentances() {
+        viewModelScope.launchWithProgress(inProgressLiveData) {
+            when (val result = repository.getSentences()) {
+                is Result.Success -> sentencesLiveData.value = result.data.sentences
+                is Result.Error -> Timber.e("Failed to fetch sentences")
             }
         }
     }
