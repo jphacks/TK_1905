8004ca1cbfe800ae208ac0a0d8c3c6857584c2f2
tks10 <tktktks10@gmail.com>
2019-10-26 12:26:25 +0900

    feat: add sign in/up

--- android/app/src/main/java/jp/co/myowndict/view/splash/SplashViewModel.kt
+++ android/app/src/main/java/jp/co/myowndict/view/splash/SplashViewModel.kt
@@ -3,20 +3,42 @@ package jp.co.myowndict.view.splash
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hadilq.liveevent.LiveEvent
+import jp.co.myowndict.data.Repository
 import jp.co.myowndict.extensions.notify
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import jp.co.myowndict.model.Result
 
-class SplashViewModel @Inject constructor() : ViewModel() {
+class SplashViewModel @Inject constructor(
+    private val repository: Repository
+) : ViewModel() {
+    fun signUp(): Pair<LiveEvent<Unit>, LiveEvent<Unit>> {
+        val onSuccess = LiveEvent<Unit>()
+        val onFailure = LiveEvent<Unit>()
 
-    val authenticated
-        get() = _authenticated
+        viewModelScope.launch {
+            when (val result = repository.signUp()) {
+                is Result.Success -> onSuccess.notify()
+                is Result.Error -> onFailure.notify()
+            }
+        }
 
-    private val _authenticated = LiveEvent<Unit>()
-
-    fun authenticate() = viewModelScope.launch {
-        delay(2000)
-        _authenticated.notify()
+        return Pair(onSuccess, onFailure)
     }
+
+    fun signIn(uuid: String): Pair<LiveEvent<Unit>, LiveEvent<Unit>> {
+        val onSuccess = LiveEvent<Unit>()
+        val onFailure = LiveEvent<Unit>()
+
+        viewModelScope.launch {
+            when (val result = repository.signIn(uuid)) {
+                is Result.Success -> onSuccess.notify()
+                is Result.Error -> onFailure.notify()
+            }
+        }
+
+        return Pair(onSuccess, onFailure)
+    }
+
+    fun isNewUser(): Boolean = repository.getUuid() == null
 }
