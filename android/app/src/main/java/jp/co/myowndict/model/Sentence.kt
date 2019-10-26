486ab05bf41616778c4e890d490d7d4db7a845dc
shiita0903 <shiita0903@gmail.com>
2019-10-27 01:10:00 +0900

    add spoken count.

--- android/app/src/main/java/jp/co/myowndict/model/Sentence.kt
+++ android/app/src/main/java/jp/co/myowndict/model/Sentence.kt
@@ -2,11 +2,10 @@ package jp.co.myowndict.model
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import kotlin.random.Random
 
 @JsonClass(generateAdapter = true)
 data class Sentence(
     @Json(name = "content_jp") val contentJp: String,
     @Json(name = "content_en") val contentEn: String,
-    val count: Int = Random.nextInt() % 20   // 用途不明。とりあえずランダム値を設定
+    @Json(name = "spoken_count") val spokenCount: Int
 )
