2ec20e7042e7155e178980f7973969ed5cd950c1
tks10 <tktktks10@gmail.com>
2019-10-27 11:52:42 +0900

    feat: animation

--- android/app/src/main/java/jp/co/myowndict/view/splash/SplashFragment.kt
+++ android/app/src/main/java/jp/co/myowndict/view/splash/SplashFragment.kt
@@ -13,14 +13,22 @@ import com.wada811.databinding.dataBinding
 import dagger.android.support.DaggerFragment
 import jp.co.myowndict.R
 import jp.co.myowndict.databinding.FragmentSplashBinding
+import kotlinx.coroutines.*
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
-class SplashFragment : DaggerFragment() {
+class SplashFragment : DaggerFragment(), CoroutineScope {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private val viewModel: SplashViewModel by viewModels { viewModelFactory }
 
     private val binding by dataBinding<FragmentSplashBinding>(R.layout.fragment_splash)
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + Job()
+
+    private var waitDone = false
+    private var entryDone = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,13 +44,26 @@ class SplashFragment : DaggerFragment() {
         } else {
             signIn()
         }
+
+        launch {
+            delay(600)
+            waitDone = true
+            navigateIfWaitingHasDone()
+        }
+    }
+
+    private fun navigateIfWaitingHasDone() {
+        if (waitDone && entryDone) {
+            findNavController().navigate(SplashFragmentDirections.actionSplashToMain())
+        }
     }
 
     private fun signUp() {
         val (onSuccess, onFailure) = viewModel.signUp()
 
         onSuccess.observe(viewLifecycleOwner, Observer {
-            findNavController().navigate(SplashFragmentDirections.actionSplashToMain())
+            entryDone = true
+            navigateIfWaitingHasDone()
         })
         onFailure.observe(viewLifecycleOwner, Observer {
             MaterialDialog(requireContext()).show {
@@ -56,7 +77,8 @@ class SplashFragment : DaggerFragment() {
         val (onSuccess, onFailure) = viewModel.signIn()
 
         onSuccess.observe(viewLifecycleOwner, Observer {
-            findNavController().navigate(SplashFragmentDirections.actionSplashToMain())
+            entryDone = true
+            navigateIfWaitingHasDone()
         })
         onFailure.observe(viewLifecycleOwner, Observer {
             MaterialDialog(requireContext()).show {
