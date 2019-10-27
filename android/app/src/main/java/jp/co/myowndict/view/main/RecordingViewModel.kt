fe95640e3969d0d287ec6bbdb1fc6e66ba3ad123
shiita0903 <shiita0903@gmail.com>
2019-10-27 11:15:41 +0900

    add Span sample.

--- android/app/src/main/java/jp/co/myowndict/view/main/RecordingViewModel.kt
+++ android/app/src/main/java/jp/co/myowndict/view/main/RecordingViewModel.kt
@@ -1,8 +1,13 @@
 package jp.co.myowndict.view.main
 
+import android.text.*
+import android.text.style.ForegroundColorSpan
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import jp.co.myowndict.MyApplication
+import jp.co.myowndict.R
 import jp.co.myowndict.data.Repository
 import jp.co.myowndict.extensions.combineLatest
 import jp.co.myowndict.extensions.launchWithProgress
@@ -12,38 +17,60 @@ import timber.log.Timber
 import javax.inject.Inject
 
 class RecordingViewModel @Inject constructor(
+    private val application: MyApplication,
     private val repository: Repository
 ) : ApiFragmentViewModel() {
-    private val partialResultLiveData: MutableLiveData<String> = MutableLiveData("")
-    private val resultLiveData: MutableLiveData<String> = MutableLiveData("")
+    private val partialResultLiveData: MutableLiveData<Spanned> =
+        MutableLiveData(SpannedString(""))
+    private val resultLiveData: MutableLiveData<Spanned> = MutableLiveData(SpannedString(""))
 
-    val partialResult: LiveData<String>
+    val partialResult: LiveData<Spanned>
         get() = partialResultLiveData
-    val result: LiveData<String>
+    val result: LiveData<Spanned>
         get() = resultLiveData
-    val recognizedSentence: LiveData<String> =
-        partialResult.combineLatest(result) { partial, result ->
-            "$result\n$partial"
-        }
+    val recognizedSentence: LiveData<Spanned> =
+        partialResult.combineLatest(result) { partial, result -> concatSpannable(result, partial) }
 
     fun sendSpeechText(text: String) {
         viewModelScope.launchWithProgress(inProgressLiveData) {
             when (val result = repository.sendText(text)) {
-                is Result.Success -> Timber.d("Sent content -> $text")
+                is Result.Success -> {
+                    Timber.d("Sent content -> $text")
+                    // 非同期処理の扱いはとりあえず考えない
+//                    partialResult.value = partialResult.value.run {
+//                        replace()
+//                    }
+                }
                 is Result.Error -> Timber.e("Failed to send content -> $text")
             }
         }
     }
 
     fun updatePartialResult(text: String) {
-        if (text.isNotEmpty()) partialResultLiveData.value = text
+        if (text.isNotEmpty()) partialResultLiveData.value = SpannedString("")
     }
 
     fun clearPartialResult() {
-        partialResultLiveData.value = ""
+        partialResultLiveData.value = SpannedString("")
     }
 
     fun addResult(text: String) {
-        resultLiveData.value = "${resultLiveData.value}\n$text"
+        resultLiveData.value = concatSpannable(
+            resultLiveData.value ?: SpannedString(""),
+            SpannedString(text)
+        )
+    }
+
+    private fun concatSpannable(s1: Spanned, s2: Spanned): Spanned {
+        val colorSpan = ForegroundColorSpan(
+            ContextCompat.getColor(
+                application,
+                R.color.colorAccent
+            )
+        )
+        val builder = SpannableStringBuilder(s1).apply {
+            setSpan(colorSpan, 0, s1.length, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+        }
+        return TextUtils.concat(builder, "\n", s2) as Spanned
     }
 }
